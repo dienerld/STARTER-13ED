1a9fa5c29e4086283387d6d3bf95879b665c6103
Leticia Leal <leticialealp@hotmail.com>
2023-04-10 18:25:33 -0300

    adiciona exercicio para aula 10.04

--- FRONT-III/react-js-aula-03.04/vite-project/src/pages/Home/index.tsx
+++ FRONT-III/react-js-aula-03.04/vite-project/src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import TitleDefault from '../../components/Heading';
 import Input from '../../components/Input';
-import Container from '../../components/Container';
+import Container from '../../components/Container/styles';
 
 const Home: React.FC = () => {
 	return (
@@ -13,6 +13,15 @@ const Home: React.FC = () => {
 				placeholder="Descreva a tarefa..."
 				type="text"
 			/>
+
+			{/* 
+
+				TO-DO
+				1 - Criar uma lista de tarefas (definir types e criar o mock de registros)
+				2 - Criar componente do Card
+				3 - Renderizar um Card para cada tarefa da lista  
+
+			*/}
 		</Container>
 	);
 };
