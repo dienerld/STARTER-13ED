80030bb0dd9ef39ec1c1c35d9928e80e9ee68191
Leticia Leal <leticialealp@hotmail.com>
2023-06-19 19:03:14 -0300

    start aula 4

--- BACK-END/aula04-19.06/src/contratos/acoes-pessoa.contract.ts
+++
@@ -1,7 +0,0 @@
-import Endereco from '../classes/endereco.class';
-
-interface AcoesPessoa {
-	atualizarDados: (nome: string, idade: number, endereco: Endereco) => void;
-}
-
-export default AcoesPessoa;
