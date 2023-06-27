b9dc843d65675ffd2a622239902f734331e6f09c
Leticia Leal <leticialealp@hotmail.com>
2023-06-26 22:33:29 -0300

    Co-authored-by: LuanFelipeCB <LuanFelipeCB@users.noreply.github.com>

---
+++ BACK-END/aula-06-26.06/src/classes/clientes.class.ts
@@ -0,0 +1,35 @@
+import Usuario, { UsuarioDTO } from './usuario.class';
+
+type ClienteDTO = UsuarioDTO & { endereco: Array<any> };
+
+class Cliente extends Usuario {
+	endereco: Array<any>;
+
+	constructor(dados: ClienteDTO) {
+		super({
+			id: dados.id,
+			nome_completo: dados.nome_completo,
+			telefone: dados.telefone,
+			cpf: dados.cpf,
+			email: dados.email,
+			senha: dados.senha,
+		});
+		this.endereco = dados.endereco;
+	}
+
+	public toJSON() {
+		const objCliente = {
+			id: this.id,
+			nome_completo: this.nome_completo,
+			telefone: this.telefone,
+			email: this.email,
+			senha: this.senha,
+			cpf: this.cpf,
+			endereco: this.endereco,
+		};
+
+		return objCliente;
+	}
+}
+
+export default Cliente;
