d5428f8744e5b9d677feef1b7a59b81683d9f55e
Leticia Leal <leticialealp@hotmail.com>
2023-06-28 18:17:02 -0300

    remove comentario

--- BACK-END/aula06-26.06/src/server.ts
+++ BACK-END/aula06-26.06/src/server.ts
@@ -2,9 +2,6 @@ import express from 'express';
 
 const app = express();
 
-// MIDDLEWARES - intermediario - Ex: autenticação de rotas
-// JSON
-
 // torna desnecessario a utilizacao do JSON.parse() e JSON.stringify()
 app.use(express.json());
 
