2f191acd0d21cea65ed8fb7ec11d5fdfe47c1ecd
Diener Dornelas <diener.ld@outlook.com>
2023-09-21 20:30:56 -0300

    inicia testes unitarios

---
+++ ARQUITETURA-SOFTWARE/api-vagas/tests/app/features/users/usecase/create.usecase.spec.ts
@@ -0,0 +1,100 @@
+import { CreateUserDTO } from '../../../../../src/app/features/users/DTO';
+import { UsersRepository } from '../../../../../src/app/features/users/repository/users.repository';
+import { CreateUserUsecase } from '../../../../../src/app/features/users/usecase/create.usecase';
+import { User } from '../../../../../src/app/models/user.model';
+import { Profile } from '../../../../../src/app/shared/enums';
+import { DatabaseConnection } from '../../../../../src/main/database/typeorm.connection';
+
+const makeSut = () => {
+  const usecase = new CreateUserUsecase();
+
+  const dto: CreateUserDTO = {
+    name: 'any_name',
+    password: 'any_password',
+    profile: Profile.ADMIN,
+    username: 'any_username',
+    company: 'any_company',
+  };
+
+  return {
+    usecase,
+    dto,
+  };
+};
+
+describe('[Usecase - User] - Create', () => {
+  jest.mock(
+    '../../../../../src/app/features/users/repository/users.repository'
+  );
+
+  beforeAll(async () => {
+    await DatabaseConnection.connect();
+  });
+
+  afterAll(async () => {
+    await DatabaseConnection.destroy();
+  });
+
+  it('should return status 200 with user created', async () => {
+    const { dto, usecase } = makeSut();
+    jest
+      .spyOn(UsersRepository.prototype, 'findByUsername')
+      .mockResolvedValueOnce(null);
+    jest
+      .spyOn(UsersRepository.prototype, 'save')
+      .mockImplementationOnce(async (userDto) => {
+        return new User(
+          'uuid',
+          userDto.username,
+          userDto.name,
+          userDto.password,
+          userDto.profile,
+          userDto.company
+        );
+      });
+
+    const response = await usecase.execute(dto);
+
+    expect(response.code).toBe(200);
+    expect(response.data).toHaveProperty('id', 'uuid');
+    expect(response.data).toHaveProperty('company', 'any_company');
+  });
+
+  it('should return status 200 with user created without company', async () => {
+    const { dto, usecase } = makeSut();
+    jest
+      .spyOn(UsersRepository.prototype, 'findByUsername')
+      .mockResolvedValueOnce(null);
+    jest
+      .spyOn(UsersRepository.prototype, 'save')
+      .mockImplementationOnce(async (userDto) => {
+        return new User(
+          'uuid',
+          userDto.username,
+          userDto.name,
+          userDto.password,
+          userDto.profile,
+          userDto.company
+        );
+      });
+    dto.company = undefined;
+    const response = await usecase.execute(dto);
+
+    expect(response.code).toBe(200);
+    expect(response.data).toHaveProperty('id', 'uuid');
+    expect(response.data.company).toBeUndefined();
+  });
+
+  it('should return status 400 when send user already exist', async () => {
+    const { dto, usecase } = makeSut();
+
+    jest
+      .spyOn(UsersRepository.prototype, 'findByUsername')
+      .mockResolvedValueOnce({} as User);
+
+    const response = await usecase.execute(dto);
+
+    expect(response.code).toBe(400);
+    expect(response.message).toBe('User already exists');
+  });
+});
