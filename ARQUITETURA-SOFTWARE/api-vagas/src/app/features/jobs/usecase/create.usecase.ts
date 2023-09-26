a654ad1416e66a78f7f5c3568bd9582b4c5485c7
Peterson C C Nied <petersonied@gmail.com>
2023-09-26 20:53:20 -0300

    Criado primeiro test de criação de jobs

--- ARQUITETURA-SOFTWARE/api-vagas/src/app/features/jobs/usecase/create.usecase.ts
+++ ARQUITETURA-SOFTWARE/api-vagas/src/app/features/jobs/usecase/create.usecase.ts
@@ -1,13 +1,13 @@
-import { Result, ResultDTO } from '../../../shared/utils/result.helper';
-import { CreateJobDTO } from '../DTO';
-import { JobsRepository } from '../repository';
+import { Result, ResultDTO } from "../../../shared/utils/result.helper";
+import { CreateJobDTO } from "../DTO";
+import { JobsRepository } from "../repository";
 
 export class CreateJobUsecase {
-	async execute(data: CreateJobDTO): Promise<ResultDTO> {
-		const repository = new JobsRepository();
+  async execute(data: CreateJobDTO): Promise<ResultDTO> {
+    const repository = new JobsRepository();
 
-		const job = await repository.save(data);
+    const job = await repository.save(data);
 
-		return Result.success(200, 'Job created successfully', job.toJSON());
-	}
+    return Result.success(200, "Job created successfully", job.toJSON());
+  }
 }
